51dc73f0705be779ae7c137b8e7968b98ffdee80
Richard Crowley <r@rcrowley.org>
2009-08-30 23:11:36 +0000

    Refactored php_install and python_install out to the new sourceinstall module.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,54 +1,3 @@
-define php_install($version, $flags) {
-
-	file { "/opt/php-$version.tar.bz2":
-		before => Exec["extract-php-$version"],
-		source => "puppet://$servername/php/php-$version.tar.bz2",
-		ensure => present,
-	}
-	exec { "extract-php-$version":
-		require => File["/opt/php-$version.tar.bz2"],
-		before => Exec["configure-php-$version"],
-		cwd => "/root",
-		command => "tar xjf /opt/php-$version.tar.bz2",
-		creates => "/root/php-$version",
-		unless => "test -d /opt/php-$version",
-	}
-
-	exec { "configure-php-$version":
-		require => Exec["extract-php-$version"],
-		before => Exec["make-php-$version"],
-		cwd => "/root/php-$version",
-		command => "./configure --prefix=/opt/php-$version $flags",
-		timeout => "-1",
-		creates => "/root/php-$version/Makefile",
-		onlyif => "test -d /root/php-$version",
-	}
-	exec { "make-php-$version":
-		require => Exec["configure-php-$version"],
-		before => Exec["install-php-$version"],
-		cwd => "/root/php-$version",
-		command => "make",
-		timeout => "-1",
-		creates => "/root/php-$version/sapi/cli/php",
-		onlyif => "test -d /root/php-$version",
-	}
-	exec { "install-php-$version":
-		require => Exec["make-php-$version"],
-		before => Exec["remove-php-$version"],
-		cwd => "/root/php-$version",
-		command => "make install",
-		creates => "/opt/php-$version",
-		onlyif => "test -d /root/php-$version",
-	}
-
-	exec { "remove-php-$version":
-		cwd => "/root",
-		command => "rm -rf php-$version",
-		onlyif => "test -d /root/php-$version",
-	}
-
-}
-
 class php {
 
 	package { "libxml2-dev": ensure => latest }
@@ -64,16 +13,22 @@ class php {
 }
 
 class php::php_5_3_0 {
-	php_install { "php-5.3.0":
+	sourceinstall { "php-5.3.0":
+		module => "php",
+		package => "php",
 		version => "5.3.0",
 		flags => "--enable-cli --enable-cgi --with-openssl --with-curl --with-zlib --enable-intl --with-mysql=mysqlnd --with-mysqli=mysqlnd --enable-pdo --with-pdo-mysql --enable-pcntl --enable-sqlite-utf8 --disable-phar --with-libxml-dir=/usr --with-pear",
+		bin => "sapi/cli/php",
 	}
 }
 
 class php::php_5_2_10 {
 	package { "libmysqlclient15-dev": ensure => latest }
-	php_install { "php-5.2.10":
+	sourceinstall { "php-5.2.10":
+		module => "php",
+		package => "php",
 		version => "5.2.10",
 		flags => "--enable-cli --enable-cgi --enable-fastcgi --with-openssl --with-curl --with-zlib  --with-mysql --with-mysqli --enable-pdo --with-pdo-mysql --enable-pcntl --enable-sqlite-utf8 --with-libxml-dir=/usr --with-pear",
+		bin => "sapi/cli/php",
 	}
 }
